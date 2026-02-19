f7ca13eab5bc2a1dbac6862a45345eb73ba4c4b1
Bogdane Braoun <112286@ecole-it.com>
2026-02-19 23:34:39 +0100

    adjusting dark and light mode for all pages

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,41 @@
 const mode_btn = document.querySelector("#color_mode")
-let theme = "d"
+let theme = "l"
 
-mode_btn.addEventListener("click", () => {
-  document.documentElement.classList.toggle("light");
-  if (theme == "d"){
+// to set light mode
+function light_mode(){
+    document.documentElement.classList.add("light");
+    document.documentElement.classList.remove("dark");
     mode_btn.textContent = "Light Mode"
     theme = "l"
-  }else{
+    localStorage.setItem("mode", theme)
+
+}
+
+// to set dark mode
+function dark_mode(){
+    document.documentElement.classList.add("dark");
+    document.documentElement.classList.remove("light");
     mode_btn.textContent = "Dark Mode"
     theme = "d"
+    localStorage.setItem("mode", theme)
+
+}
+
+// Click on change colore mode button
+mode_btn.addEventListener("click", () => {
+  if (theme == "d"){
+    light_mode()
+  }else{
+    dark_mode()
   }
 });
+// On page load
+addEventListener("DOMContentLoaded", () =>{
+    console.log(localStorage["mode"])
+    if(localStorage["mode"] == "l"){
+        light_mode()
+    }else{
+        dark_mode()
+    }
+})
+
